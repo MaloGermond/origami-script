fcc328ecdaa0c504b707076594b54e63ff478988
MaloGermond <github@matchalo.org>
2023-11-21 16:14:53 +0100

    [feat] add parallax, default and gitignore

---
+++ default.js
@@ -0,0 +1,33 @@
+//==============================================================================
+// Welcome to scripting in Origami! Helpful links:
+//
+// Scripting Basics - https://origami.design/documentation/concepts/scriptingbasics
+// Scripting API - https://origami.design/documentation/concepts/scriptingapi
+//
+// Script ID: 2CBD541A-E9FA-487B-9B79-BEB5B8206034
+//==============================================================================
+
+
+// Define your patch
+
+var patch = new Patch();
+
+// Patches are always being evaluated when inputs change of values. If you need your patch to run every frame set this to true
+// Setting this to true makes scripts very inefficient and should be avoided at all cost.
+patch.alwaysNeedsToEvaluate = false;
+
+// Set Inputs and Outputs.
+patch.inputs = [
+  new PatchInput("Input", types.NUMBER, 0),
+];
+
+patch.outputs = [
+  new PatchOutput("Output", types.NUMBER),
+];
+
+// Add your logic in this function.
+patch.evaluate = function() {
+  patch.outputs[0].value = patch.inputs[0].value;
+}
+
+return patch;
